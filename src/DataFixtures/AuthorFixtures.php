6389ac0930c880e78aee7219ec4076c441aa0dec
pazpok <mathebossdu22@hotmail.fr>
2018-11-22 16:13:30 +0100

    Affichage dynamique sur le front

--- src/DataFixtures/AuthorFixtures.php
+++ src/DataFixtures/AuthorFixtures.php
@@ -1,27 +1,35 @@
 <?php
-
 namespace App\DataFixtures;
-
 use App\Entity\Author;
+use App\Entity\Book;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-
+use Faker\Factory;
 class AuthorFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         $authors = [
-            ["J.K", "Rowling"],
-            ["Gustave", "Flaubert"],
+            ["J.K.", "Rowling"],
             ["René", "Goscinny"],
-            ["Emile", "Zola"]
+            ["Gustave", "Flaubert"],
+            ["Emile", "Zola"],
         ];
-        foreach ($authors as $author) {
+        foreach ($authors as $key => $author) {
             $aut = new Author();
             $aut->setFirstname($author[0]);
             $aut->setLastname($author[1]);
             $manager->persist($aut);
-            $this->setReference("author-" . strtolower($author[1]), $aut);
+            $this->setReference('author-' . ($key + 1), $aut);
+        }
+        $faker = Factory::create('fr_FR');
+        for($i = 5; $i < 100; $i++) {
+            $faker->seed($i);
+            $author = new Author();
+            $author->setFirstname($faker->firstName);
+            $author->setLastname($faker->lastName);
+            $manager->persist($author);
+            $this->setReference('author-' . $i, $author);
         }
         $manager->flush();
     }
--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -1,21 +1,18 @@
 <?php
-
 namespace App\DataFixtures;
-
 use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-
 class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         $categories = ["Roman", "SF", "BD"];
-        foreach ($categories as $category) {
+        foreach ($categories as $key => $category) {
             $cat = new Category();
             $cat->setName($category);
             $manager->persist($cat);
-            $this->setReference('category-' . strtolower($category), $cat);
+            $this->setReference('category-' . ($key + 1), $cat);
         }
         $manager->flush();
     }
